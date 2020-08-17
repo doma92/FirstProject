0611df1b7d6e842a66e37433317fb49c275b77f2
Domokos Szegő <szegodomokos@gmail.com>
2020-08-17 21:29:55 +0200

    Few more changes added

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -8,11 +8,17 @@ namespace FirstProject
 {
     class Program
     {
+        static int Add(int n, int m)
+        {
+            return n + m;
+        }
         static void Main(string[] args)
         {
           
             Console.WriteLine("This is the first app that demonstrates ");
             Console.WriteLine("Hello Git!");
+
+            Console.WriteLine("78 + 13 = " + Add(78,13));
 
             Console.ReadLine();
             
